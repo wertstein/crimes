1f6fe12ccd7b83c5aca7944dbcd72cdf20dd539d
Andriy Vankovych <a.vankovych@gmail.com>
2019-08-21 00:06:15 +0300

    Addd caching

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,10 @@
-import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import {
+  Module,
+  MiddlewareConsumer,
+  RequestMethod,
+  CacheModule,
+  CacheInterceptor,
+} from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,17 +13,27 @@ import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { config } from './config/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
     CrimesModule,
+    CacheModule.register({
+      ttl: 5, // seconds
+      max: 10, // maximum number of items in cache
+    }),
     AuthModule,
     UsersModule,
     MongooseModule.forRoot(config.mongo),
   ],
-
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
